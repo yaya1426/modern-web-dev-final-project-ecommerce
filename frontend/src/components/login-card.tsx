084a0156435c5b2c9434b364ff25aa0984d4f50d
Yahya Nabil Salah <yaya1426@gmail.com>
2023-10-07 16:06:35 +0300

    Add clerk logic

---
+++ frontend/src/components/login-card.tsx
@@ -0,0 +1,15 @@
+"use client";
+import styled from "styled-components";
+
+const LoginCard = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 544px;
+  height: 474px;
+
+  background: #ffffff;
+  box-shadow: 0px 0px 25px 10px #f8f8fb;
+`;
+
+export default LoginCard;
